51327741e1a4477e42441247e9de3b6b85bcc305
marzi <marzieh.nourmohammadi@gmail.com>
2025-08-17 01:55:28 +0330

    added empty bookmark module

---
+++ app/src/main/java/com/marziehnourmohamadi/productlist/ui/bookmark/BookmarkScreen.kt
@@ -0,0 +1,10 @@
+package com.marziehnourmohamadi.productlist.ui.bookmark
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun BookmarkScreen(navController: NavController, viewModel: BookmarkViewModel = hiltViewModel()) {
+
+}
